8cc560b07b9b095fe06cc76223087793885ab93d
Jacson <jeremias.jacson@gmail.com>
2020-03-12 23:44:18 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa :
-    def __init__(self, nome = None, idade = 35):
+    def __init__(self, *filhos, nome = None, idade = 35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
         
     def cumprimentar(self):
         return f'ola {id(self)}'
 
 
 if __name__ == "__main__":
-    p = Pessoa('Pedro')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Jacson'
-    print(p.nome)
-    print(p.idade)
+    joao = Pessoa(nome = 'João')
+    jacson = Pessoa(joao, nome = 'Jacson')
+    print(Pessoa.cumprimentar(jacson))
+    print(id(jacson))
+    print(jacson.cumprimentar())
+    print(jacson.nome)
+    print(jacson.idade)
+    for filho in jacson.filhos:
+        print(filho.nome)
