14b2bc1bbcd9f891fb51ed563eb84e3d1812d200
Jacson <jeremias.jacson@gmail.com>
2020-03-15 14:18:46 -0300

    Criado atributo de classe ano_atual e o método para descobrir o ano de nascimento comm base na idade da pessoa

--- oo/pessoa_dois.py
+++ oo/pessoa_dois.py
@@ -1,16 +1,18 @@
-
+from datetime import datetime
 
 
 """Exemplo programação a objetos canal Otávio Miranda - Aula 35, onde é criada uma classe Pessoa, atributos e métodos
 """
 class Pessoa:
-    def __init__(self, nome, idade, comendo = False, falando=False):
+    ano_atual = int(datetime.strftime(datetime.now(), '%Y'))            #Atributo de classe, é igual para todos os obejtos criados, usar quando o valor for igual para todos os objetos
+
+    def __init__(self, nome, idade, comendo = False, falando=False):    #Atributo de instância valor independente de cada objeto criado, usar quando for algo com valor diferente entre os objetos
         self.nome = nome
         self.idade = idade
         self.comendo = comendo
         self.falando = falando
 
-    def comer(self, alimento):
+    def comer(self, alimento):                                          #método do atributo
         if self.comendo:
             print(f'{self.nome} já esta comendo ')
             return
@@ -19,7 +21,6 @@ class Pessoa:
             return
         print(f'{self.nome} esta comendo {alimento}')
         self.comendo = True
-
 
     def parar_comer(self):
         if self.comendo:
@@ -38,8 +39,6 @@ class Pessoa:
         print(f'{self.nome} esta comendo não pode falar')
         self.comendo = False
 
-
-
     def parar_falar(self):
         if self.falando:
             print(f'{self.nome} parou de falar')
@@ -49,12 +48,21 @@ class Pessoa:
             print(f'{self.nome} já não esta falando pois esta comendo')
             return
 
-p1 = Pessoa('Jacson', 22 )
+    def get_ano_nascimento(self):
+        self.ano_nascimento = self.ano_atual - self.idade
+        print(f'{self.nome} nasceu em {self.ano_nascimento}')
+
+
+p1 = Pessoa('Jacson', 33 )
 p2 = Pessoa('Diego', 90 )
 
+print('--------------------------')
 p1.falar('POO')
 p1.parar_comer()
+p1.get_ano_nascimento()
+print('--------------------------')
 p2.comer('Maçã')
 p2.parar_falar()
+p2.get_ano_nascimento()
 
 
